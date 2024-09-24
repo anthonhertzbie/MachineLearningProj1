67a0134a9a550b9810b0a6a0436fecd3dc037fce
Aiden_Gabriel <gabrieai@oregonstate.edu>
2024-09-24 16:30:39 +0200

    Added PC3 to PCA Component Coefficients Plot

--- visualization.py
+++ visualization.py
@@ -59,7 +59,7 @@ def find_coeff(normalized_df, attribute_labels):
     V = Vt.T
     N, M = normalized_df.shape
 
-    pcs = [0, 1]
+    pcs = [0, 1, 2]
     legendStrs = ["PC" + str(e + 1) for e in pcs]
     bw = 0.2
     r = np.arange(1, M + 1)
