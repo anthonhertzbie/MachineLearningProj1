884a33aa7dce6cdda20379d43c5de0ce9c8229cf
Aiden_Gabriel <gabrieai@oregonstate.edu>
2024-09-24 11:23:50 +0200

    Improved Histogram Plots

--- dist.py
+++ dist.py
@@ -3,12 +3,19 @@ import matplotlib.pyplot as plt
 
 def histograms(normalized_df, attribute_labels):
     num_cols = normalized_df.shape[1]
+
+    _, axes = plt.subplots(2, 4, figsize=(12, 6))
+    axes = axes.flatten()
+
     for col in range(num_cols):
         col_data = normalized_df[:, col]
 
-        # Create histogram
-        plt.hist(col_data, bins=5, edgecolor='black') 
-        plt.xlabel('Value')
-        plt.ylabel('Frequency')
-        plt.title(f'Histogram of Attribute {attribute_labels[col]}')
-        plt.show()
+        ax = axes[col]
+
+        ax.hist(col_data, bins=8, edgecolor='black') 
+        ax.set_xlabel('Value')
+        ax.set_ylabel('Frequency')
+        ax.set_title(f'Histogram of Attribute {attribute_labels[col]}')
+        
+    plt.tight_layout(pad=2.0)
+    plt.show()
